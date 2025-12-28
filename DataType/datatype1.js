5593d2e8f841f48602b4d90c5e27b2dd99c1a207
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-12-28 17:55:05 +0000

    Udemy Js

--- DataType/datatype1.js
+++ DataType/datatype1.js
@@ -1,2 +1,34 @@
 
-console.log("Hello Monu Yadav")
+// console.log("Hello Monu Yadav")
+
+// Data Type 
+
+/*
+Total data type 8 
+
+String 
+Number
+Boolean
+BigInt
+Undefined
+Null
+Object
+Symbol
+
+*/
+
+// var score = 102
+
+let score = 102 
+
+let name = "Zentrox Tom"
+
+let isLoggedIn = false 
+
+// object
+let teaType = ["lemon tea", "black tea", "Herbal tea"] 
+let user = {firstname:"Zentrox", lastname:"Tom"}
+
+let getscore = score
+
+console.log(getscore);
---
+++ DataType/datatype2.js
@@ -0,0 +1,11 @@
+
+
+// let gamename = "Spiderman";
+// gamename = "Batman"
+// console.log(gamename)
+
+
+
+// const name = "Zentrox"
+// name = "Tom"
+// console.log(name)  // error becoused const 
