e9d2e8b6bf49f4795c186364d3b97c7bce6384a7
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-04 05:28:14 +0000

    js

---
+++ DataType/datatype3.js
@@ -0,0 +1,11 @@
+
+// number convert 
+
+let num = "11"       // if "11a" --> NaN output 
+
+let convertnumber = Number(num)
+console.log(convertnumber)
+console.log(typeof convertnumber)
+console.log(typeof num)
+
+
