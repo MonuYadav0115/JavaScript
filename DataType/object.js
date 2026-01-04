f7d33abb756688df120a9f5a41f49b154bf6f947
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-04 16:33:24 +0000

    js

--- DataType/object.js
+++ DataType/object.js
@@ -42,6 +42,39 @@
 
 // important ---->
 
-let {name ,ingredients} = chaiRecipe    // ist called object destruturing
-let [firstchai , secondchai] = chaiType  // its called array destruring 
+// let {name ,ingredients} = chaiRecipe       // ist called object destruturing
+// let [firstchai , secondchai] = chaiType    // its called array destruring 
 
+
+
+
+// 3 user check login or not throgh the usename and password 
+// && ---> means (and)
+
+
+// function login(username,password){
+//     if(username === "zentrox" && password === "0115" ){
+//         console.log("User login successfull")
+//     }else{
+//         console.log("user credintiale not valid")
+//     }
+// }
+// login("zentrox","1115")
+
+
+
+// 4 check user loging valid or not username and password and iPaddress (password or ipaddress one is right so user able to login)
+
+
+// ------>(||)---->means (or)
+
+
+function login(username,password,ipaddress){
+    if(username ==="zentrox" && (password === "0115" && ipaddress === 123)){
+        console.log("User loggedIn")
+    }else{
+        console.log("user credentials")
+    }
+}
+
+login("zentrox","0115",1223)
