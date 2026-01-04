f644900786ee8f0e149fdf696400a4f1093fcafa
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-04 11:59:21 +0000

    js

---
+++ DataType/array1.js
@@ -0,0 +1,55 @@
+// array 
+
+// let nameTypes = ["Zentrox","Tom","Monu","bravix"]
+
+// console.log(nameTypes[0])
+// console.log(`toatal length of name: ${nameTypes}`)
+
+// nameTypes.push("Monu Tom")
+// console.log(nameTypes)
+// const newname = nameTypes.pop()
+
+// console.log(newname)
+
+
+
+// 2 
+
+// let index = nameTypes.indexOf("Monu")
+
+// if(index !== -1){
+//     nameTypes.splice(index,1)
+// }
+// console.log(nameTypes)
+
+
+
+// 3 
+
+
+// let nameTypes = ["Zentrox","Tom","Monu","bravix"]
+
+// nameTypes.forEach((name,index) =>{
+//     console.log(`${index} : ${name}`);
+// });
+
+
+// 4 add two array  
+
+
+// let nameTypes = ["Zentrox","Tom","Monu","bravix"]
+
+// let morename = ["sonu","vikash","vishal"]
+
+// let allname = nameTypes.concat(morename)
+// console.log(allname)
+
+
+// 5  add new name in array through spred method 
+
+let nameTypes = ["Zentrox","Tom","Monu","bravix"]
+
+let newname = [...nameTypes, "Monu Yadav"]
+
+console.log(newname)
+
