0c963af5c15cca12b9496601761a0856095bdba3
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-04 12:22:44 +0000

    js

---
+++ DataType/object.js
@@ -0,0 +1,47 @@
+
+// Object 
+
+// let chaiRecipe  = {
+//     name: "Masala chai",
+//     ingrediants: {
+//         teaLeaves:"Assam tea",
+//         milk:"Full cream milk",
+//         sugar:"brown sugar",
+//         spices:["Daalchini","Ginger"]
+//     },
+//     instruction:"Boil water ,add tea leaves milk,sugar and spices"
+
+// }
+
+// console.log(chaiRecipe.name)
+// console.log(chaiRecipe.ingrediants.spices[1])
+
+
+
+// 2  updated instruction 
+
+
+//  let chaiRecipe  = {
+//     name: "Masala chai",
+//     ingrediants: {
+//         teaLeaves:"Assam tea",
+//         milk:"Full cream milk",
+//         sugar:"brown sugar",
+//         spices:["Daalchini","Ginger"]
+//     },
+//     instruction:"Boil water ,add tea leaves milk,sugar and spices"
+
+// }
+
+// let updatedchaiRecipe = {
+//     ...chaiRecipe,
+//     instruction: "Please make a chai full of energy and make a beautifull chai "
+// }
+// console.log(updatedchaiRecipe)
+
+
+// important ---->
+
+let {name ,ingredients} = chaiRecipe    // ist called object destruturing
+let [firstchai , secondchai] = chaiType  // its called array destruring 
+
