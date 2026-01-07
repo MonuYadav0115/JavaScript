8c0b998fcd297ec4cca007a857cf8e0c35a20c1a
Monu Tom <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-07 10:39:18 +0000

    js

---
+++ loop_practice1/loop2.js
@@ -0,0 +1,30 @@
+
+// write a while loop that counts down from 5 to 1 and store the number in array name coutdown
+
+// let countdown = []
+
+// let num = 5;
+
+// while(num > 0){
+//     countdown.push(num)
+//     num--
+
+// }
+// console.log(countdown)
+
+
+
+
+
+// countdown 0 to 10 and add new array 
+
+
+let countdown = []
+
+let startnum = 0
+
+while(startnum <= 10){
+    countdown.push(startnum);
+    startnum++
+}
+console.log(countdown)
