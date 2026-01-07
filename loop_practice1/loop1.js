68c535f35fa6b82dd1d6f4c4a1a277f7f0ff89a4
Monu Tom <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-07 10:32:50 +0000

    js

---
+++ loop_practice1/loop1.js
@@ -0,0 +1,23 @@
+
+/* 1-> :  Write a while loop that calculate the sum of all numbers 1 to 5 and store result in sum valiable : */
+
+
+// let sum = 0;
+// let i = 0;
+
+// while(i <= 10){
+//     sum = sum + i
+//     i++
+// }
+// console.log(sum)
+
+// sum of number 
+
+let sum = 0;
+let i = 0;
+
+while(i <= 5){
+    sum = sum + i
+    i++
+}
+console.log(sum)
