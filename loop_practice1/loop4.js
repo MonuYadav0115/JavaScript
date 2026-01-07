d3013e36559589d8c905de2f642456e20fba0d02
Monu Tom <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-07 11:05:05 +0000

    js

---
+++ loop_practice1/loop4.js
@@ -0,0 +1,17 @@
+
+// write a program do while loop that adds numbers from 1 to 3 and stores the result in a variable name total:
+
+
+let total = []
+
+let num = 0
+
+do{
+   total = total + num
+   
+   num++
+
+}while(num <=3)
+
+console.log(total)
+
