0440a379277b047517584e52a52644935dfe9697
Monu Tom <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-09 08:13:57 +0000

    js

---
+++ loop_practice1/loop6.js
@@ -0,0 +1,12 @@
+// 
+
+/* 1-> :  Write a while loop that calculate the sum of all numbers 1 to 5 and store result in sum valiable : */
+
+// let num = 0 ;
+// let sum = 0 ;
+
+// while(num <= 5){
+//     sum = sum + num
+//     num++
+// }
+// console.log(sum)
---
+++ loop_practice1/loop7.js
@@ -0,0 +1,24 @@
+
+/* 2 :
+write a while loop that counts down from 5 to 1 and store the number in array name coutdown */
+
+// let countdown = []
+
+// let num = 1;
+
+// while(num <= 5){
+//     countdown.push(num)
+//     num++
+// }
+// console.log(countdown)
+
+
+let countdown = []
+let num = 5;
+
+while(num > 0){
+    countdown.push(num)
+    num--
+}
+
+console.log(countdown)
