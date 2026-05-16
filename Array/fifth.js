207b151e3b15707945e0a65e18c3c07741945fad
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-05-16 04:37:07 +0000

    array

---
+++ Array/fifth.js
@@ -0,0 +1,8 @@
+// Array.isArray:To check if the data type is an array
+
+const numbers = [1,2,3,4,5]
+console.log(Array.isArray(numbers))
+
+
+const number = 100
+console.log(Array.isArray(number))
