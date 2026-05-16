0015c9b6ffb1b462176322f6e70b1fbd47881288
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-05-16 04:34:09 +0000

    array

---
+++ Array/fourth.js
@@ -0,0 +1,22 @@
+// includes:To check if an item exist in an array. If it exist it returns the true else it returns false.
+
+const numbers = [1,2,3,4,5,6]
+
+console.log(numbers.includes(5))
+console.log(numbers.includes(0))
+console.log(numbers.includes(1))
+console.log(numbers.includes(7))
+console.log(numbers.includes(7))
+ 
+const webTech = [
+    "HHTML",
+    "CSS",
+    "JAVA SCRIPT",
+    "REACT",
+    "REDUX",
+    "NODE",
+    "MONGODB",
+]   
+console.log("Web Tech Output here...")
+console.log(webTech.includes("NODE"))
+console.log(webTech.includes("C++"))
