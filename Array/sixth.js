478ea176e558655b2cc4dccf701bc521d5b3a2b4
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-05-16 04:41:08 +0000

    array

---
+++ Array/sixth.js
@@ -0,0 +1,11 @@
+// toString:Converts array to string
+
+const numbers = [1,2,3,4,5]
+console.log(numbers)
+console.log(numbers.toString())
+
+const names = ["Asabeneh","Mathias","Elias","Brook"]
+console.log(names.toString())
+console.log(names)
+
+
