f102531511e1b784c895399d9b79cb39618daf9c
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-05-16 04:19:16 +0000

    array

---
+++ Array/first.js
@@ -0,0 +1,16 @@
+// Length:To know the size of the array
+
+const number = [1,2,3,4,5]
+console.log("number of length:",number.length)
+
+
+// Getting index of an element in an array
+// indexOf:To check if an item exist in an array. If it exists it returns the index else it returns -1.
+
+const numbers = [1,2,3,4,5]
+console.log(numbers.indexOf(5))
+console.log(numbers.indexOf(2))
+console.log(numbers.indexOf(0))
+console.log(numbers.indexOf(1))
+console.log(numbers.indexOf(6))
+
