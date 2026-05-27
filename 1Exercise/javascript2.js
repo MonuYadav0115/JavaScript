4b527ec8d15624b3a4bc6667d4f6bbe8253fb24a
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-05-27 06:13:32 +0000

    Exersice 2

---
+++ 1Exercise/javascript2.js
@@ -0,0 +1,13 @@
+let myAge = 23 
+
+let yourAge = Number(prompt("Enter Your age!"))
+
+if (myAge > yourAge){
+    console.log("I am Older than you")
+
+}else if (yourAge > myAge){
+    console.log("You are older then me")
+} else{
+    console.log("Both are same age!")
+    
+}
