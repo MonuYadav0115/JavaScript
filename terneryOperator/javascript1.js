01f357122f279864c785ab7b5f64796257121793
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-05-27 05:37:00 +0000

    terneryoperator

---
+++ terneryOperator/javascript1.js
@@ -0,0 +1,6 @@
+let isRaining = false
+isRaining
+   ? console.log("You need a raincoat!")
+   : console.log("No need for raircoat!")
+
+// example--->>   (? if condition true or : else condition false )
