42dac936010470acf55c52b711f9677805befcc9
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-05-28 05:03:45 +0000

    for loop

---
+++ forloopPractice2/javascript6.js
@@ -0,0 +1,11 @@
+// The Code below can reverse an array.
+
+
+const nums = [1,2,3,4,5]
+
+const lastarray = num.length - 1
+const newArray = []
+for(let i = lastarray; i >= 0; i--){
+    newArray.push(num[i])
+} 
+console.log(newArray)
